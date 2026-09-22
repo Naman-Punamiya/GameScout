cbefe082bba3f1ba8f711e79f6160b6f8926ef19
Naman-Punamiya <namanpunamiya1@gmail.com>
2026-09-22 09:16:43 +0530

    Initial Adding

---
+++ backend/src/main/java/com/ndevstudios/gamescout/GamescoutApplication.java
@@ -0,0 +1,13 @@
+package com.ndevstudios.gamescout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamescoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamescoutApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ndevstudios/gamescout/GamescoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndevstudios.gamescout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamescoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
